890664f94c318afb718e16bf87491ceff6495a9d
Nico Prat <nicooprat@gmail.com>
2017-12-16 15:41:38 +0100

    Cleanup

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>AlterÉcolo</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: normal;
+  }
+</style>
