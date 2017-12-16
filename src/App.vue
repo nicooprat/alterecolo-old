d5205383222041562ec1608bbd82914a9a01d08c
Nico Prat <nicooprat@gmail.com>
2017-12-16 18:13:54 +0100

    CSS enhancements

--- src/App.vue
+++ src/App.vue
@@ -10,20 +10,35 @@
   }
 </script>
 
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+<style lang="scss">
+  @import './scss/vars';
+
+  * {
+    line-height: 1.418;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+  }  
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
     color: #2c3e50;
     overflow: hidden;
     padding: 0 1em;
-    max-width: 50em;
+    max-width: 45em;
     margin: auto;
   }
 
   h1, h2, h3, h4, h5, h6 {
     font-weight: normal;
     color: DodgerBlue;
+  }
+
+  a {
+    color: $blue;
+    
+    &:hover,
+    &:focus {
+      color: mix(white, $blue, 40);
+    }
   }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,18 +2,31 @@
   <div class="hello">
     <h1>AlterÉcolo</h1>
 
-    <ul class="list" v-if="items">
-      <li class="item" v-for="item in items">
+    <ul class="list" v-if="getItems">
+      <li class="item" v-for="item in getItems">
         <img class="cover" v-if="item.cover" :src="item.cover.thumbnails.large.url" :alt="item.Alternative" :width="item.cover.thumbnails.large.width" :height="item.cover.thumbnails.large.height">
+        <span class="cover" v-else></span>
         <span v-if="item.Difficulté" class="difficulty">
-          <svg v-for="n in 3" :class="{on: item.Difficulté >= n}" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
+          <svg v-for="n in 3" :class="{off: item.Difficulté < n}" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
         </span>
         <small class="subtitle">{{item.Remplacé}}</small>
         <strong class="title">{{item.Alternative}}</strong>
         <nav class="links" v-if="item.Catégorie">
           <a class="category" href="#" v-for="category in item.Catégorie">{{category}}</a>          
-          <a class="source" v-if="item.Lien" :href="item.Lien">Source</a>
-          <a class="expand" v-if="item.Lien" href="#" v-on:click.prevent="expand(item.id)">Plus d'infos</a>
+          <a class="source" v-if="item.Lien" :href="item.Lien">
+            Source
+            <svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/></svg>
+          </a>
+          <a class="expand" v-if="item.Lien" href="#" v-on:click.prevent="expand(item.id)">
+            <span v-if="item.expanded">
+              Moins d'infos
+              <svg width="24" height="24" viewBox="0 0 24 24"><path transform="rotate(270) translate(-24,0)" fill="currentColor" d="M8.122 24l-4.122-4 8-8-8-8 4.122-4 11.878 12z"/></svg>
+            </span>
+            <span v-else>
+              Plus d'infos
+              <svg width="24" height="24" viewBox="0 0 24 24"><path transform="rotate(90) translate(0,-24)" fill="currentColor" d="M8.122 24l-4.122-4 8-8-8-8 4.122-4 11.878 12z"/></svg>
+            </span>
+          </a>
         </nav>
         <span v-show="item.expanded" class="desc">{{item.Description}}</span>
       </li>
@@ -29,7 +42,8 @@
     name: 'Home',
     data() {
       return {
-        items: []
+        items: [],
+        categories: []
       }
     },
     created() {
@@ -42,15 +56,17 @@
         // Fetch new items
         base('Alternatives').select({
           filterByFormula: '{Validé} = 1' // Only manually validated items
-        }).eachPage((records, fetchNextPage) => {
+        }).eachPage((items, fetchNextPage) => {
           // Load items in component
-          this.items.push(...records.map((item) => {
+          this.items.push(...items.map((item) => {
+            this.categories.push(...item.fields.Catégorie)
             return {
               id: item.id,
               cover: item.fields.Photo && item.fields.Photo[0], // Easier access
               expanded: false, // Toggle visibility, must be declared to be reactive
               ...item.fields,
             }
+            // Push categories
           }))
           // Load all datas
           fetchNextPage()
@@ -91,6 +107,10 @@
     object-fit: cover;
     margin-top: -1em;
     margin-left: -7.5em;
+    border: 3px solid white;
+    border-radius: 5px;
+    overflow: hidden;
+    background-color: rgba(black,.03);
   }
 
   .difficulty {
@@ -100,10 +120,16 @@
       display: inline-block;
       width: 18px; height: 18px;
       margin-left: .25em;
-      color: rgba(black,.1);
+      color: gold;
 
-      &.on {
-        color: gold;        
+      &.off {
+        color: rgba(black,.1);
+
+        path {
+          fill: none;
+          stroke: currentColor;
+          stroke-width: 2px;
+        }
       }
     }
   }
@@ -112,6 +138,7 @@
     display: block;
     font-size: 1em;
     opacity: .75;
+    margin-bottom: .5em;
   }
 
   .title {
@@ -121,29 +148,24 @@
 
   .links {
     font-size: .9em;
+    margin-top: .5em;
 
     > a {
-      color: $blue;
-      display: inline-block;
-      margin-right: .5em;
+      display: inline-flex;
+      align-items: center;
+      margin-right: 1em;
+      text-decoration: none;
+
+      svg {
+        width: 0.7em; height: 0.7em;
+        margin-left: .25em;
+      }
     }
   }
 
   .category {
-    text-decoration: none;
     font-weight: bold;
-    position: relative;
-    z-index: 0;
-    white-space: nowrap;
-
-    &:before {
-      content: '';
-      position: absolute;
-      bottom: .1em; left: -.25em; right: -.25em;
-      height: .5em;
-      background: currentColor;
-      opacity: .07;
-    }
+    border-bottom-width: 2px !important;
   }
 
   .desc {
@@ -152,7 +174,6 @@
     margin-top: 0;
     margin-left: -7.5em;
     padding: 1em;
-    background-color: mix(black, white, 3);
     position: relative;
     z-index: 1;
     clear: left;
