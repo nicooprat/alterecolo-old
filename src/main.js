c950859f2acc496869ad5206b94b6afc9ac9805a
Nico Prat <nicooprat@gmail.com>
2017-12-17 14:50:16 +0100

    Bugsnag

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import settings from './settings'
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
 import VueFuse from 'vue-fuse'
+import bugsnag from 'bugsnag-js'
+import bugsnagVue from 'bugsnag-vue'
+
+const bugsnagClient = bugsnag(settings.bugsnag)
+bugsnagClient.use(bugsnagVue(Vue))
 
 Vue.use(VueFuse)
 
