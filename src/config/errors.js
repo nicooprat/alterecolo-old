75788726d02691cf25457466c81c46040eccd323
Nico Prat <nicooprat@gmail.com>
2017-12-17 19:18:25 +0100

    Commit Bugsnag public key

--- src/config/errors.js
+++ src/config/errors.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import bugsnag from 'bugsnag-js'
 import bugsnagVue from 'bugsnag-vue'
 
-const bugsnagClient = bugsnag(settings.bugsnag)
+const bugsnagClient = bugsnag('3fa623ae462ce13db539a7a70631a0fa')
 bugsnagClient.use(bugsnagVue(Vue))
 
 export default bugsnagClient
