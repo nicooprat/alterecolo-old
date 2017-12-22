0dc4b87fa4dc0cc1b53eabbb80c0fedfa6452404
Nico Prat <nicooprat@gmail.com>
2017-12-22 14:02:54 +0100

    Fix sort date order

--- src/components/List.vue
+++ src/components/List.vue
@@ -88,7 +88,7 @@
             items = items.sort((a, b) => a.Difficulté > b.Difficulté ? 1 : -1)
             break
           case 'date':
-            items = items.sort((a, b) => a.createdTime > b.createdTime ? 1 : -1)
+            items = items.sort((a, b) => a.createdTime > b.createdTime ? -1 : 1)
             break
           default:
             if (this.$store.state.search) items = items.sort((a, b) => a.score > b.score ? 1 : -1)
