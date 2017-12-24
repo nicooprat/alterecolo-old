8e9e3947bcdc8bfb1ed330ee837b213f62f36895
Nico Prat <nicooprat@gmail.com>
2017-12-24 13:51:33 +0100

    Disqus comments

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,18 +1,29 @@
 <template>
   <article v-if="item">
     <Item :item="item"/>
-    <p>{{item.Description}}</p>
+    <blockquote>
+      <p>{{item.Description}}</p>
+      <cite v-if="item.Lien">
+        <a :href="item.Lien">
+          {{item.Lien}}
+        </a>
+        <svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/></svg>
+      </cite>
+    </blockquote>
+    <vue-disqus shortname="alterecolo"></vue-disqus>
   </article>
   <main v-else>404</main>
 </template>
 
 <script>
   import Item from '@/components/Item'
+  import VueDisqus from 'vue-disqus/VueDisqus.vue'
 
   export default {
     name: 'Details',
     components: {
       Item,
+      VueDisqus
     },
     props: ['item'],
     computed: {},
@@ -23,6 +34,33 @@
 
 <style scoped lang="scss">
   @import '../scss/vars';
+
+  blockquote {
+    margin: 0;
+    margin-bottom: 2em;
+  }
+
+  cite {
+    display: flex;
+    align-items: center;
+    margin-top: 1em;
+    font-size: .9em;
+
+    a {
+      font-style: normal;
+      text-decoration: none;
+      border-bottom: 1px solid;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+
+    svg {
+      width: 0.75em; height: 0.75em;
+      margin-left: .5em;
+      color: $blue;
+    }
+  }
 
   p {
     white-space: pre-wrap;
